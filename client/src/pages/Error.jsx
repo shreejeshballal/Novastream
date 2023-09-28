95155f8ec049685f66455336ab79f816b44bda82
KarthikJR <karthikjrao69@gmail.com>
2023-09-28 11:57:39 +0530

    Error page

---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Space1,Space } from "../assets";
+
+function Error() {
+	return (
+		<div className="h-[100vh] overflow-hidden">
+			<img
+				src={Space}
+				className="brightness-[0.4] h-[100vh] w-full object-cover absolute -z-10"
+				alt=""
+			/>
+			<div className="w-full h-full flex flex-col items-center justify-center">
+				<img src="/logo.svg" alt="logo" className="h-14 cursor-default" />
+				<h1 className="text-9xl font-bold text-light my-3 cursor-default">
+					404
+				</h1>
+				<h3 className="text-xl text-light-gray my-3 text-center cursor-default">
+					Looks like you have headed to the wrong page
+				</h3>
+                <button className="bg-dark-gray my-3 px-5 py-2 rounded-md text-light-gray hover:bg-light-gray hover:text-dark-gray transition-all cursor-pointer">
+                    Take me to the home page
+                </button>
+			</div>
+		</div>
+	);
+}
+
+export default Error;
