1203ee6c10bd2ec049207d9b317c12dea502f246
Shreejesh J Ballal <shreejeshballal@gmail.com>
2023-09-28 12:23:30 +0530

    Added tray functionality

--- client/src/components/TagTray.jsx
+++ client/src/components/TagTray.jsx
@@ -1,17 +1,42 @@
 import { useState, useEffect } from "react";
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 const TagTray = () => {
-  const trayItems = [
-    "All",
-    "Gaming",
-    "Sports",
-    "Gaming",
-    "Technology",
-    "Education",
-    "Movies",
-    "Songs",
-    "Reading",
-  ];
+  const [trayItems, setTrayItems] = useState([
+    {
+      id: 1,
+      name: "All",
+      active: false,
+    },
+    {
+      id: 2,
+      name: "Gaming",
+      active: false,
+    },
+    { id: 3, name: "Songs", active: false },
+    {
+      id: 4,
+      name: "Education",
+      active: false,
+    },
+    { id: 5, name: "Sports", active: false },
+    { id: 6, name: "Motivation", active: false },
+    { id: 7, name: "Technology", active: false },
+    { id: 8, name: "Fitness", active: false },
+    { id: 9, name: "Space", active: false },
+  ]);
+
+  const handleTrayItems = (id) => {
+    trayItems.forEach((element) => {
+      if (element.id === id) {
+        element.active = !element.active;
+      }
+    });
+    const sortedArray = trayItems.sort((a, b) => {
+      return b.active - a.active || a.id - b.id;
+    });
+    setTrayItems([...sortedArray]);
+  };
+
   const [scrollActive, setScrollActive] = useState(false);
 
   useEffect(() => {
@@ -44,7 +69,7 @@ const TagTray = () => {
       >
         {scrollActive && (
           <div
-            className="p-2  transition-all absolute h-full  bg-background z-10 "
+            className="p-2  transition-all absolute h-full flex justify-center items-center bg-background z-10 "
             onClick={scrollLeft}
           >
             <AiOutlineLeft className=" right-0 text-light text-xl" />
@@ -53,10 +78,13 @@ const TagTray = () => {
         {trayItems.map((item, key) => {
           return (
             <div
+              onClick={() => handleTrayItems(item.id)}
               key={key}
-              className="bg-dark-gray py-2 w-fit px-3 rounded-lg flex items-center justify-center hover:bg-light-gray transition-all cursor-pointer "
+              className={`bg-dark-gray py-2 w-fit px-3 rounded-lg flex items-center justify-center hover:bg-light-gray transition-all cursor-pointer ${
+                item.active ? "bg-light-gray text-dark-gray" : ""
+              }}`}
             >
-              {item}
+              {item.name}
             </div>
           );
         })}
--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -2,6 +2,12 @@ import { MdUpload } from "react-icons/md";
 import SearchBar from "../components/SearchBar";
 import TagTray from "../components/TagTray";
 const Navbar = () => {
+
+
+
+
+
+
   return (
     <nav className="w-full bg-background h-[5rem] md:gap-10 flex items-center px-6 justify-between fixed z-10  sm:px-10 md:px-7 gap-5">
       <img
