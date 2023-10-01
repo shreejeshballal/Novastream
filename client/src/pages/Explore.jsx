6c7d2b35f7354feac8946905be2bff886fce96c2
KarthikJR <karthikjrao69@gmail.com>
2023-10-01 21:41:02 +0530

    Updated explore page
    Items centered

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -4,9 +4,9 @@ import Card from "../components/Card";
 
 function Explore() {
 	return (
-		<div className="flex gap-10 flex-row flex-wrap">
+		<div className="flex gap-10 flex-row flex-wrap justify-center">
 			{Videos.map((videos) => (
-				<Card key={videos.id} video={videos.video} />
+					<Card key={videos.id} video={videos.video} />
 			))}
 		</div>
 	);
