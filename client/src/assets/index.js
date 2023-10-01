022b31e69724ff539f871b8b6c85a7957d6b0c45
KarthikJR <karthikjrao69@gmail.com>
2023-10-01 17:25:38 +0530

    Added dummy Video content and half done home page

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,6 @@
 import Avatar from './avatar.png'
 import errorLight from './errorLight.png'
 import errorDark from './errorDark.png'
+import Videos from './Sample';
 
-export { Avatar, errorLight, errorDark };
+export { Avatar, errorLight, errorDark, Videos };
