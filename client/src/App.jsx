5b5a9b471edd4881c5fc50790e6af0e8b01a58ce
Shreejesh J Ballal <shreejeshballal@gmail.com>
2023-10-07 21:11:10 +0530

    changes names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Toaster } from "sonner";
-
+import Home from "./pages/Home/Home";
 import Navbar from "./layout/Navbar/Navbar";
 import Sidebar from "./layout/Sidebar";
 import Error from "./pages/error/Error";
-import Home from "./pages/home/Home.jsx";
 import AuthModal from "./features/authentication/AuthModal";
 import { AuthModalProvider } from "./context/AuthModalContext.jsx";
 import Container from "./layout/Container";
 import Upload from "./features/Upload/Upload";
 import Explore from "./pages/explore/Explore";
-import Subscription from "./pages/sub/Subscription";
+import Subscription from "./pages/subscription/Subscription";
 import Favorite from "./pages/favorite/Favorite.jsx";
 
 function App() {
