d0e3673e13edf8b3775b3bbba614854621ba4ad1
KarthikJR <karthikjrao69@gmail.com>
2023-10-07 16:01:18 +0530

    Completed home page

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,39 +1,39 @@
 const Card = ({ video }) => {
   return (
-    <div className="m-6 text-white font-dmSans flex flex-col gap-2  ">
-      <div className="relative">
-        <img
-          className="rounded-md w-96 h-60 object-cover"
-          src={video.thumbnail}
-          alt={video.title}
-        />
-        <div className="absolute bottom-0 right-0 mx-1.5 my-1 p-1 rounded-lg bg-opacity-70 bg-black">
-          {video.timestamp}
-        </div>
-      </div>
-      <div className="flex">
-        <img
-          className="h-12 w-12 mx-2 my-2 rounded-full mr-3 object-cover"
-          src={video.uploadedByImage}
-        />
-        <div className="min-w-0">
-          <div className="text-lg line-clamp-2 leading-snug">{video.title}</div>
-          <div className=" text-[#aaaaaa] text-sm font-light">
-            <div className="">
-              <span>{video.uploadedBy}</span>
-            </div>
-            <div>
-              <span>{video.viewCount} views</span> •
-              <span className="justify-right">
-                {" "}
-                {video.uploadDate} years ago
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+		<div className="m-6 w-96 text-white font-dmSans flex flex-col gap-2  ">
+			<div className="relative">
+				<img
+					className="rounded-md w-96 h-60 object-cover"
+					src={video.thumbnail}
+					alt={video.title}
+				/>
+				<div className="absolute bottom-0 right-0 mx-1.5 my-1 p-1 rounded-lg bg-opacity-70 bg-black">
+					{video.timestamp}
+				</div>
+			</div>
+			<div className="flex">
+				<img
+					className="h-12 w-12 mx-2 my-2 rounded-full mr-3 object-cover"
+					src={video.uploadedByImage}
+				/>
+				<div className="min-w-0">
+					<div className="text-lg line-clamp-2 leading-snug">{video.title}</div>
+					<div className=" text-[#aaaaaa] text-sm font-light">
+						<div className="">
+							<span>{video.uploadedBy}</span>
+						</div>
+						<div>
+							<span>{video.viewCount} views</span> •
+							<span className="justify-right">
+								{" "}
+								{video.uploadDate} years ago
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
